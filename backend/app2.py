26b3ce23eaa3c4793f2627ddc0a34fa088f48d89
Mayank-Shrivastava-1981 <shrivastavamas@gmail.com>
2025-07-18 20:29:04 +0530

    added options.binary_location = "/usr/bin/google-chrome" in app2 file

--- backend/app2.py
+++ backend/app2.py
@@ -95,6 +95,7 @@ def extract_xpaths():
         options.add_argument('--headless')
         options.add_argument('--disable-gpu')
         options.add_argument('--no-sandbox')
+        options.binary_location = "/usr/bin/google-chrome"
         service = Service(ChromeDriverManager().install())
         driver = webdriver.Chrome(service=service, options=options)
 
