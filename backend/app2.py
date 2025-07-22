aec3a05020e6c7d040710367c85f70eded52aef9
Mayank-Shrivastava-1981 <shrivastavamas@gmail.com>
2025-07-22 12:04:36 +0530

    updated port number

--- backend/app2.py
+++ backend/app2.py
@@ -121,4 +121,4 @@ def extract_xpaths():
         return jsonify({'error': str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=8000)
